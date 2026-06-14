ca161c7569e938e40852e91675a9e9dc6a49af5b
Shaina Sarika <130648106+Shaina-Sarika@users.noreply.github.com>
2026-06-14 22:13:32 +0530

    Improve get_chapter_list function in chatbot_utility.py
    Refactor get_chapter_list to handle non-existent directories and filter for PDF files.

--- src/chatbot_utility.py
+++ src/chatbot_utility.py
@@ -1,19 +1,20 @@
 import os
 
-
 working_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(working_dir)
 
 def get_chapter_list(selected_subject):
+    subject_name = selected_subject.lower()
 
-    if selected_subject == "Biology":
-        subject_name = selected_subject.lower()
-        chapters_dir = f"{parent_dir}/data/class_12/{subject_name}"
-        chapters_list = os.listdir(chapters_dir)
-        chapters_list = [x[:-4] for x in chapters_list]
-        chapters_list.sort(key=lambda x: int(x.split('.')[0]))
-        return chapters_list
+    chapters_dir = f"{parent_dir}/data/class_12/{subject_name}"
 
+    if not os.path.exists(chapters_dir):
+        return []
 
-# chapters_list = get_chapter_list("Biology")
-# print(chapters_list)
+    chapters_list = [
+        x[:-4] for x in os.listdir(chapters_dir)
+        if x.endswith(".pdf")
+    ]
+
+    chapters_list.sort(key=lambda x: int(x.split(".")[0]))
+    return chapters_list
